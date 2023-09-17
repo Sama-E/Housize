9c9ef303f271f3e15ffcdbd74a1621af86d37201
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-09-16 21:27:05 -0400

    theme color update

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -39,92 +39,120 @@ const BarChart = ({ isDashboard = false }) => {
             fill: colors.grey[100],
           },
         },
+        tooltip: {
+          container: {
+            color: colors.primary[500],
+          },
+        },
       }}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
+      keys={[
+        'hot dog',
+        'burger',
+        'sandwich',
+        'kebab',
+        'fries',
+        'donut'
+      ]}
       indexBy="country"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
-      valueScale={{ type: "linear" }}
-      indexScale={{ type: "band", round: true }}
-      colors={{ scheme: "nivo" }}
+      valueScale={{ type: 'linear' }}
+      indexScale={{ type: 'band', round: true }}
+      colors={{ scheme: 'nivo' }}
       defs={[
-        {
-          id: "dots",
-          type: "patternDots",
-          background: "inherit",
-          color: "#38bcb2",
-          size: 4,
-          padding: 1,
-          stagger: true,
-        },
-        {
-          id: "lines",
-          type: "patternLines",
-          background: "inherit",
-          color: "#eed312",
-          rotation: -45,
-          lineWidth: 6,
-          spacing: 10,
-        },
+          {
+              id: 'dots',
+              type: 'patternDots',
+              background: 'inherit',
+              color: '#38bcb2',
+              size: 4,
+              padding: 1,
+              stagger: true
+          },
+          {
+              id: 'lines',
+              type: 'patternLines',
+              background: 'inherit',
+              color: '#eed312',
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10
+          }
+      ]}
+      fill={[
+          {
+              match: {
+                  id: 'fries'
+              },
+              id: 'dots'
+          },
+          {
+              match: {
+                  id: 'sandwich'
+              },
+              id: 'lines'
+          }
       ]}
       borderColor={{
-        from: "color",
-        modifiers: [["darker", "1.6"]],
+          from: 'color',
+          modifiers: [
+              [
+                  'darker',
+                  1.6
+              ]
+          ]
       }}
       axisTop={null}
       axisRight={null}
       axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: isDashboard ? undefined : "country", // changed
-        legendPosition: "middle",
-        legendOffset: 32,
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: isDashboard ? undefined : 'country', // disable for dashboard
+          legendPosition: 'middle',
+          legendOffset: 32
       }}
       axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: isDashboard ? undefined : "food", // changed
-        legendPosition: "middle",
-        legendOffset: -40,
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: isDashboard ? undefined : "food", // disable for dashboard
+          legendPosition: 'middle',
+          legendOffset: -40
       }}
       enableLabel={true}
       labelSkipWidth={12}
       labelSkipHeight={12}
       labelTextColor={{
-        from: "color",
-        modifiers: [["darker", 1.6]],
+          from: 'color',
+          modifiers: [
+              [
+                  'darker',
+                  1.6
+              ]
+          ]
       }}
       legends={[
-        {
-          dataFrom: "keys",
-          anchor: "bottom-right",
-          direction: "column",
-          justify: false,
-          translateX: 120,
-          translateY: 0,
-          itemsSpacing: 2,
-          itemWidth: 100,
-          itemHeight: 20,
-          itemDirection: "left-to-right",
-          itemOpacity: 0.85,
-          symbolSize: 20,
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemOpacity: 1,
-              },
-            },
-          ],
-        },
+          {
+              dataFrom: 'keys',
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemsSpacing: 2,
+              symbolSize: 20,
+              itemDirection: 'left-to-right'
+          }
       ]}
+      // isInteractive={false}
       role="application"
-      barAriaLabel={function (e) {
-        return e.id + ": " + e.formattedValue + " in country: " + e.indexValue;
-      }}
-    />
+      isFocusable={true}
+      ariaLabel="Nivo bar chart demo"
+      barAriaLabel={e=>e.id+": "+e.formattedValue+" in country: "+e.indexValue}
+  />
   )
 }
 
